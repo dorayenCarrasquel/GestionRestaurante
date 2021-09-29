c0e6e51c1d28d0d89fdb3726ce06e08c9e8a2b57
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-09-29 10:02:57 -0300

    Entrega minima

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -15,9 +15,14 @@ public class Sistema {
         System.out.println("\n\t\tM E N U");
         System.out.println("\n===================================");
         System.out.println("[1] Cadastrar Prato");
-        System.out.println("[2] cadastrar Cardapio");
-        System.out.println("[3] Exibir Cardapio");
+        System.out.println("[2] Exibir Cardapio");
         System.out.println("[4] Sair");
+    }
+    public static Prato cadastrarPrato(){
+        String nome = capturarDados("Digite o nome do prato: ").nextLine();
+        double valor = capturarDados("Digite o valor do prato: ").nextDouble();
+        Prato prato = new Prato(nome,valor);
+        return prato;
     }
     public static Ingrediente cadastrarIngredientes(){
        String nome = capturarDados("Digite um Ingrediente: ").nextLine();
@@ -26,18 +31,39 @@ public class Sistema {
        return ingredientes;
 
     }
-    /*public static Prato ListandoOsIngrediente(){
-        String cantIngredientes = capturarDados("Digite cantidad de Ingredientes a adicionar: ").nextInt();
-        List<Ingrediente>ingredientesDoPrato =new ArrayList<>();
+
+
+    public static Prato ListandoOsIngrediente(){
+        int cantIngredientes = capturarDados("Digite cantidad de Ingredientes a adicionar: ").nextInt();
 
         for (int i = 0; i < cantIngredientes; i++) {
-        Ingrediente ingredientes = cadastrarIngredientes();
-        return
+        cadastrarIngredientes();
+        return;
         }
-*/
+
     }
     public static void executar(){
         menu();
+        Cardapio cardapio = new Cardapio();
+
+        int opcaoMenu =true
+
+        while (opcaoMenu){
+            int opcao = capturarDados("Digite uma opcão: ").nextInt();
+            if (opcao ==1){
+                Prato prato = cadastrarPrato();
+                cardapio.adicionarPrato(prato);
+                prato.adicionarIngrediente(cadastrarIngredientes());
+            }
+            else if (opcao == 2){
+                System.out.println(cardapio);
+
+            }else if (opcao==3){
+                opcaoMenu=false;
+                System.out.println("Comio mucho é!!!");
+            }
+        }
+
 
 
 
