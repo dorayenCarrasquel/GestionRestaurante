0218ac70ac7ca3d1ae67aa15938b70e1d2a8deb2
Dorayen José Arreaza Carrasquel <dorayen.carrasquel@zup.com.br>
2021-09-29 09:06:50 -0300

    Adelantandome en los tiempos (abortando lista...)por ahora..

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -1,10 +1,12 @@
 package br.com.zup;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Sistema {
     private static Scanner capturarDados(String mensagem){
-        System.out.println(memsagem);
+        System.out.println(mensagem);
         return new Scanner(System.in);
     }
 
@@ -18,14 +20,26 @@ public class Sistema {
         System.out.println("[4] Sair");
     }
     public static Ingrediente cadastrarIngredientes(){
-       String nome = capturarDados("Digite um Ingrediente").nextLine();
+       String nome = capturarDados("Digite um Ingrediente: ").nextLine();
        Ingrediente ingredientes = new Ingrediente(nome);
+
        return ingredientes;
 
     }
-    public static void
+    /*public static Prato ListandoOsIngrediente(){
+        String cantIngredientes = capturarDados("Digite cantidad de Ingredientes a adicionar: ").nextInt();
+        List<Ingrediente>ingredientesDoPrato =new ArrayList<>();
+
+        for (int i = 0; i < cantIngredientes; i++) {
+        Ingrediente ingredientes = cadastrarIngredientes();
+        return
+        }
+*/
+    }
     public static void executar(){
         menu();
+
+
 
 
     }
